8d8ccc9c1fe8f2f73514b361546d950fda9441f4
said <saiderdemozturk@gmail.com>
2024-04-13 19:17:43 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    //constants - sabitler
+    public static class Messages
+    {
+        public static string ProductAddedd = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string ProductListed = "Ürünler listelendi" ;
+        public static string MaintenceTime = "Bakım zamanı";
+        public static string ProductNameAlreadyExists = "Böyle bir ürün ismi zaten mevcut.";
+        public static string AuthorizationDenied = "Yetkisiz işlem";
+        internal static string UserRegistered = "Üye olma başarılı";
+        internal static string UserNotFound = "Kullanıcı bulunamadı";
+        internal static string PasswordError = "Şifre hatası";
+        internal static string SuccessfulLogin = "Giriş başarılı";
+        internal static string UserAlreadyExists = "Kullanıcı mevcut";
+        internal static string AccessTokenCreated ="Token oluşturuldu.";
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //ATTRIBUTE  -- JAVADA ANNOTATION   // class ile ilgili bilgi vermek için kullanılır.
+    public class ProductsController : ControllerBase
+    {
+        //IoC container -- Inversion of Control Bir liste var. Bu liste içerisinde bütün newleme işlemlerimizi yapıyoruz.
+
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+    }
+}
